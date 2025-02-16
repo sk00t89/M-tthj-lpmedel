0985a40053570a447c98ddecf05ff0b4065326b4
sk00t89 <mark.eriksson89@gmail.com>
2025-02-16 15:15:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const takInput = document.getElementById("takmått");
+const vVInput = document.getElementById("vänster-vägg");
+const hVInput = document.getElementById("höger-vägg");
+const bInput = document.getElementById("botten");
+const outputList = document.getElementById("måttlista");
+const generateBtn = document.getElementById("generera-btn");
+const clearBtn = document.getElementById("rensa-btn");
+const removeItem = document.getElementById("remove-btn");
+const checkbox = document.getElementById("checkbox");
+const select = document.getElementById("select-menu");
+
+const generateMeasure = (input1, input2, input3, input4) => {
+    if (input1 && input2 && input3 && input4 === "") {}
+}
